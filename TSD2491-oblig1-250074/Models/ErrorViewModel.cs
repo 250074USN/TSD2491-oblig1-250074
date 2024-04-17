01b0c47044654e6dc234d6860fd42b555a604821
johems <250074@usn.no>
2024-04-17 22:53:07 +0200

    Dette er initial commit

---
+++ TSD2491-oblig1-250074/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TSD2491_oblig1_250074.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
